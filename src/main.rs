304cecfce3964dba95e273982e8e5bb4d5131262
Sani <javiersanisidrogarcia@gmail.com>
2021-05-14 09:32:53 +0200

    get all of database incomplete

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,10 @@ struct AppState {
     app_name: String,
     pool: Arc<Pool<MysqlConnectionManager>>,
 }
+struct Personas{
+    pub id: u32,
+    pub name: String,
+}
 
 async fn firstget(req: HttpRequest) ->impl Responder{
     let name = req.match_info().get("name").unwrap_or("World");
@@ -54,6 +58,30 @@ async fn index(info: web::Path<i32>, data: web::Data<AppState>) -> impl Responde
  
     let unwrap_rec = rec.unwrap();
     format!("Hello {} ({})! \n from {}",  unwrap_rec.1, unwrap_rec.0, app_name)
+}
+#[get("/persona")]
+async fn index2( data: web::Data<AppState>) -> impl Responder {
+    let app_name = &data.app_name; // <- get app_name
+ 
+    let pool = &data.pool;
+ 
+ 
+    let pool = pool.clone();
+    let mut conn = pool.get().unwrap();
+    let qr=conn.query("select person_id, person_name from person").unwrap();
+    //  let qr: QueryResult = conn.prep_exec("select person_id, person_name from person where person_id = ?", (param, )).unwrap();
+    let mut listado: Vec<Personas>=Vec::new();
+    for row in qr {
+        let identificador= row.get(0);
+        let nombre= row.get(1);
+        let patata =Personas{
+            id: identificador,
+            name: nombre,
+        };
+        listado.push(patata);
+    }
+
+    HttpResponse::Ok().json(listado)
 }
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
