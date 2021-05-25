96b03f0bb39c1d959e5d10428b073538fa5e2df3
Sani <javiersanisidrogarcia@gmail.com>
2021-05-25 16:17:16 +0200

    fixed apm-server

--- index.js
+++ index.js
@@ -2,14 +2,8 @@
 var apm = require('elastic-apm-node').start({
   // Override service name from package.json
   // Allowed characters: a-z, A-Z, 0-9, -, _, and space
-  serviceName: 'practica',
-
-  // Use if APM Server requires a token
-  secretToken: '',
-
-  // Set custom APM Server URL (default: http://localhost:8200)
-  serverUrl: ''
-})
+  serviceName: 'practica'
+});
 
   const app = require('express')()
   app.get('/', function (req, res) {
